2360b320668a9e38ea040b1de130a2b4db936cd2
David Bergsten <b22davbe@student.his.se>
2023-04-26 11:29:57 +0200

    Gjort layouten till MainActivity och gjort så att submit-knappen tar en till SecondActivity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity<MyCurrentActivity> extends AppCompatActivity {
+    Button submit;
+    EditText namefield;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        submit = findViewById(R.id.my_button);
+        namefield = findViewById(R.id.editTextTextPersonName);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", namefield.getText().toString());
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
+
